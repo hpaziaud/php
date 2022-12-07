eb4f9f9e47fbd232becf4c26230062b14184dd17
hpaziaud <paziaud.hassan@gmail.com>
2022-12-07 14:10:12 +0100

    l'exo php

--- premier.php
+++ premier.php
@@ -7,8 +7,31 @@
     <title>hassan exo 1</title>
 </head>
 <body>
-ukkhkh bk klnlnlk
-ojpopo ouououu
+<?php 
+   
+   $Variable1 = "Une variable";
+   ?>
+           <div>
+           <?php            
+
+           $Variable1 = rand(0,100);
+           ?>
+
+           <div>
+               <?php
+               echo $Variable1;
+
+               if($Variable1%2==1){
+                   echo '<div class="var1">
+                   <h1>je suis impaire</h1>
+               </div>';
+
+               }else{
+                   echo '<div class="var2">
+                   <h1>je suis paire</h1>
+               </div>';
+               }
+               ?>
 
 </body>
 </html>
