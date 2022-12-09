6dbb8e88b41f71f3d868c41d48718cd9e04fda28
hpaziaud <paziaud.hassan@gmail.com>
2022-12-09 16:07:06 +0100

    changes

--- test.php
+++ test.php
@@ -6,8 +6,31 @@
 </head>
 
 <body>
-<?php echo "coucou" ; ?>
-"r"'r'efetrttr
+<?php 
+   
+    $Variable1 = "Une variable";
+    ?>
+            <div>
+            <?php            
+
+            $Variable1 = rand(0,100);
+            ?>
+
+            <div>
+                <?php
+                echo $Variable1;
+
+                if($Variable1%2==1){
+                    echo '<div class="var1">
+                    <h1>je suis impaire</h1>
+                </div>';
+
+                }else{
+                    echo '<div class="var2">
+                    <h1>je suis paire</h1>
+                </div>';
+                }
+                ?>
 </body>
 
 </html>
