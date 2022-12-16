8ada0496180cd48b830b48f6ef04c16cea472164
hpaziaud <paziaud.hassan@gmail.com>
2022-12-16 20:07:30 +0100

    g

--- test base.php
+++ test base.php
@@ -18,7 +18,16 @@ $nomBase= "hassan paziaud";
 $loginPrivbilege="SiteWeb";
 $passPrivilege="SiteWeb";
 
-$GLOBALS["pdo"] = new PDO('mysql:host=')
+$GLOBALS["pdo"] = new PDO('mysql:host=' .$ipserver . ';dbname=' . $nomBase . '', $loginPrivilege, $passPrivilage);
+
+$requete = "select * from patient";
+$resultat = $GLOBALS["pdo"]->query($requete);
+
+if ($resultat ->rowcount() >0){
+    while($tab = $resultat->fetch()){
+        echo "le nom est:" .$tab["nom"]." le prenom est: ".$tab["prenom"]."id :".$tab["id"]."<br>";
+    }
+}
 
 }catch (\throwable $th) {
 
