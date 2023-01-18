4ea1ac9aa7f640c729b3408bfd0c3ad6bbf931b9
hpaziaud <paziaud.hassan@gmail.com>
2023-01-18 15:15:45 +0100

    new

--- exo1TP2.php
+++ exo1TP2.php
@@ -1,1 +1,26 @@
-<?php echo "cocou" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo "cocou" 
+<table>
+    <thead>
+        <tr>
+            <th colspan="2">The table header made by hassan paziaud</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>The table body</td>
+            <td>with two columns</td>
+        </tr>
+    </tbody>
+</table>
+?>
+</body>
+</html>
