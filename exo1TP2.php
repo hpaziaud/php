76979aa081742ffb20855f87545cf565b3bab0aa
hpaziaud <paziaud.hassan@gmail.com>
2023-01-20 15:59:44 +0100

    last

--- exo1TP2.php
+++ exo1TP2.php
@@ -1,12 +1,4 @@
 
-<?php
-   try{
-      $pdo=new PDO("mysql:host=localhost;dbname=mabase","user","1234");
-   }
-   catch(PDOException $e){
-      echo $e->getMessage();
-   }
-?>
 
 
 
@@ -41,21 +33,21 @@
 
     <body>
         <?php
-        
-            echo "cocou coucou";
-         function mafonction($colone1,$colone2,$colone3){
+        echo 'cocou coucou';
+        function mafonction($colone1, $colone2, $colone3)
+        {
             ?>
             <table style="border: 1px solid #333;background-color:powderblue;">
             <thead >
                 <tr >
-                    <th colspan="2"> <?php echo"The table header made by hassan paziaud"; ?></th>
+                    <th colspan="2"> <?php echo 'The table header made by hassan paziaud'; ?></th>
                 </tr>
             </thead>
             <tbody>
                 <tr >
-                    <td style="border: 1px solid #333;"><?=$colone1?></td>
-                    <td style="border: 1px solid #333;"><?=$colone2?></td>
-                    <td style="border: 1px solid #333;"><?=$colone3?></td>
+                    <td style="border: 1px solid #333;"><?= $colone1 ?></td>
+                    <td style="border: 1px solid #333;"><?= $colone2 ?></td>
+                    <td style="border: 1px solid #333;"><?= $colone3 ?></td>
                 </tr>
                 <tr >
                     <td style="border: 1px solid #333;">The table body</td>
@@ -69,12 +61,14 @@
                 </tr>
             </tbody>
         </table>
-        <?php }?>
+        <?php
+        }
+        ?>
       
 
-        <?php 
-        mafonction("nom","prenoms","age");
-        mafonction("fruit","legumes","17");
+        <?php
+        mafonction('nom', 'prenoms', 'age');
+        mafonction('fruit', 'legumes', '17');
         ?>
         
   
@@ -83,30 +77,59 @@
 
 
       /*  <?php
-    $table = array(2, 4, 6, 8);
-    print_r ($table);
-    echo "somme de la table = " . array_sum($table) . "\n";
-    $moyenne = array_sum($table)/count($table);
-    echo "moyenne de la table = ". $moyenne . "\n";
+      $table = [2, 4, 6, 8];
+      print_r($table);
+      echo 'somme de la table = ' . array_sum($table) . "\n";
+      $moyenne = array_sum($table) / count($table);
+      echo 'moyenne de la table = ' . $moyenne . "\n";
+      ?>*/
 
-       ?>*/
+<?php
+$table = [2, 4, 6, 8];
+$somme_notes = 0;
+$i = 0;
+foreach ($table as $cle => $valeur) {
+    $i++; // On incrémente la variable qui nous dit combien de tour on fait
+    $somme_notes += $valeur;
+    // équivaut a $somme_notes = $somme_notes + $valeur
+}
+$moyenne = $somme_notes / $i;
+print_r($table);
+echo 'moyenne de la table = ' . $moyenne . "\n";
+echo 'il y a ' . $cle . 'nombre';
+?>
+ 
+ 
+<?php try {
+    session_start();
+    @$pass=$_POST["pass"];
+    @$valider=$_POST["valider"];
+    if(isset($valider)){
+        if(empty($pass)) $erreur="Mot de passe laissé vide!";
+        else{
+            $sel=[r,o,o,t];
+            if $sel->execute(array($pass));
+            $_SESSION["autoriser"]="oui";
+         
+      }
+      else
+         $erreur="Mauvais login ou mot de passe!";
+         echo "il y a ". $erreur ."merci";
+   }
 
-<?php 
-        $table = array(2, 4, 6, 8);
-        $somme_notes = 0;
-        $i = 0;
-        foreach($table as $cle=>$valeur)
-        {
-            $i++; // On incrémente la variable qui nous dit combien de tour on fait
-            $somme_notes+=$valeur;
-            // équivaut a $somme_notes = $somme_notes + $valeur
-        }
-        $moyenne = $somme_notes / $i;
-        print_r ($table);
-        echo "moyenne de la table = ". $moyenne . "\n";
-        echo "il y a ". $cle ."nombre";
-        ?>
 
+
+} catch (PDOException $e) {
+    echo $e->getMessage();
+} ?>
+
+
+
+ <h1>Inscription</h1>
+     <form name="fo" method="post" action="">
+         <input type="password" name="pass" placeholder="<?php echo $pass?>" /><br />
+         <input type="submit" name="valider" value="<?php echo $valider?>" />
+      </form>
 
 
 
