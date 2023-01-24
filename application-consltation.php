276a2fd7957eadb89333d5765d88adc289587e12
hpaziaud <paziaud.hassan@gmail.com>
2023-01-24 08:58:44 +0100

    bdd

--- application-consltation.php
+++ application-consltation.php
@@ -10,10 +10,35 @@
     <title>Document</title>
 </head>
 <body>
- <?php  
-echo"coucou"
-?>
 
+
+<?php echo "<h1>coucou je suis pres et vous ?</h1>";
+
+try {
+
+    $ipserver ="192.168.65.60";
+    $nomBase = "Medecin";
+    $loginPrivilege ="root";
+    $passPrivilege ="root";
+
+    $GLOBALS["pdo"] = new PDO('mysql:host=' . $ipserver . ';dbname=' . $nomBase . '', $loginPrivilege, $passPrivilege);
+
+    $requete = "select * from Patient";
+
+    $resultat = $GLOBALS["pdo"]->query($requete);
+    //resultat est du coup un objet de type PDOStatement
+    $tabMedecins = $resultat->fetchALL();
+    foreach ($tabMedecins as $Medecin) {
+       echo $Medecin["nom"]." ".$Medecin["prenom"]."<br>";
+    }
+
+    
+
+} catch (Exception  $error) {
+    echo "error est : ".$error->getMessage();
+}
+
+?>
 
 
 
