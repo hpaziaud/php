b6f290895f3d37a93d94bd7b6cbea2fb35d290a7
hpaziaud <paziaud.hassan@gmail.com>
2023-02-06 17:24:11 +0100

    tj

--- application.php
+++ application.php
@@ -38,16 +38,20 @@
     if (isset($_POST["valider"])) {
 
      
-      $requete3 = "SELECT `id` FROM `user` WHERE user.nom =. ".$_POST['surname'].";";
-      $resultat3 = $GLOBALS["pdo"]->query($requete3);
-      $tabidUser = $resultat3->fetchALL();
-      $idener = $tabidUser
-
-      echo "name = " . $_POST["name"] . " nom = " . $_POST["surname"] . "id = " . $idener . "\n";
+     
 
       $requete = "INSERT INTO `user` (`nom`, `prenom`) VALUES ('" . $_POST['surname'] . "', '" . $_POST["name"] . "')";
       $resultat = $GLOBALS["pdo"]->query($requete);
       $name = $_POST['name'];
+
+
+      $requete3 = "SELECT `id` FROM `user` WHERE user.nom =".$_POST['surname'].";";
+      $resultat3 = $GLOBALS["pdo"]->query($requete3);
+      $userID = $resultat3->fetch();
+
+      echo "name = " . $_POST['name'] . " nom = " . $_POST['surname']. "ID = ".$userID.".";
+
+      
     } else {
       echo "ca ne marche pas";
     }
