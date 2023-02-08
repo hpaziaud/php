0cd1e8352dfa06d40fc6d9959006f072f87a026b
hpaziaud <paziaud.hassan@gmail.com>
2023-02-08 12:44:39 +0100

    bdbh

--- application.php
+++ application.php
@@ -1,4 +1,6 @@
-<!DOCTYPE html>
+<?php 
+session_start();
+?><!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -7,7 +9,60 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
-
+<style>
+      html, body {
+      display: flex;
+      justify-content: center;
+      font-family: Roboto, Arial, sans-serif;
+      font-size: 15px;
+      }
+      form {
+      border: 5px solid #f1f1f1;
+      }
+      input[type=text], input[type=password] {
+      width: 100%;
+      padding: 16px 8px;
+      margin: 8px 0;
+      display: inline-block;
+      border: 1px solid #ccc;
+      box-sizing: border-box;
+      }
+      button {
+      background-color: #8ebf42;
+      color: white;
+      padding: 14px 0;
+      margin: 10px 0;
+      border: none;
+      cursor: grabbing;
+      width: 100%;
+      }
+      h1 {
+      text-align:center;
+      fone-size:18;
+      }
+      button:hover {
+      opacity: 0.8;
+      }
+      .formcontainer {
+      text-align: left;
+      margin: 24px 50px 12px;
+      }
+      .container {
+      padding: 16px 0;
+      text-align:left;
+      }
+      span.psw {
+      float: right;
+      padding-top: 0;
+      padding-right: 15px;
+      }
+      /* Change styles for span on extra small screens */
+      @media screen and (max-width: 300px) {
+      span.psw {
+      display: block;
+      float: none;
+      }
+    </style>
 <body>
 
   <?php
@@ -37,21 +92,18 @@
 
     if (isset($_POST["valider"])) {
 
-     
-     
+
+
 
       $requete = "INSERT INTO `user` (`nom`, `prenom`) VALUES ('" . $_POST['surname'] . "', '" . $_POST["name"] . "')";
       $resultat = $GLOBALS["pdo"]->query($requete);
+      $_SESSION["idUser"] = $GLOBALS["pdo"]->lastInsertId();
+      echo "l'id du user est".$_SESSION["idUser"]. "\n";
       $name = $_POST['name'];
 
 
-      $requete3 = "SELECT `id` FROM `user` WHERE user.nom =".$_POST['surname'].";";
-      $resultat3 = $GLOBALS["pdo"]->query($requete3);
-      $userID = $resultat3->fetch();
 
-      echo "name = " . $_POST['name'] . " nom = " . $_POST['surname']. "ID = ".$userID.".";
-
-      
+      echo "name = " . $_POST['name'] . " nom = " . $_POST['surname'] . ".";
     } else {
       echo "ca ne marche pas";
     }
@@ -60,19 +112,21 @@
 
 
     <?php
-    session_start();
+
     if (!isset($_SESSION['count'])) {
       $_SESSION['count'] = 0;
     } else {
       $_SESSION['count']++;
     }
     ?>
-    <p>
-      Bonjour visiteur, vous avez vu cette page <?php echo $_SESSION['count']; ?> fois.
-    </p>
+    
 
-    <h2>HTML Form name Attribute.</h2>
+    <h1>HTML Form name Attribute.</h1>
+   
     <form action="" method="POST">
+    <div class="formcontainer">
+    <hr/>
+    <div class="container">
       First name:
 
       <input type="text" name="surname">
@@ -87,7 +141,7 @@
       <input type="submit" name="valider" value=''>
 
     </form>
-
+    </div>
 
     <?php
     if (isset($_POST['valider'])) {
@@ -100,20 +154,32 @@
 
       if ($user_choice == $computer_choice) {
         echo "Egalité !";
+        $requete5 = "INSERT INTO `match` (`gagneNGP`, `date`, `idUser`) VALUES ('N', current_timestamp(), '" .$_SESSION["idUser"]. "');";
+        $resultat5 = $GLOBALS["pdo"]->query($requete5);
+        $tabuser5 = $resultat5->fetchALL();
       } elseif (
         $user_choice == "pierre" && $computer_choice == "ciseaux" ||
         $user_choice == "papier" && $computer_choice == "pierre" ||
-        $user_choice == "ciseaux" && $computer_choice == "papier"
-        
-      ) {
+        $user_choice == "ciseaux" && $computer_choice == "papier")
+      {  
+        $requete4 = "INSERT INTO `match` (`gagneNGP`, `date`, `idUser`) VALUES ('G', current_timestamp(), '" .$_SESSION["idUser"]. "');";
+    $resultat4 = $GLOBALS["pdo"]->query($requete4);
+    $tabuser4 = $resultat4->fetchALL();
+
+       
 
         echo "Vous avez gagné !";
       } else {
+        $requete6 = "INSERT INTO `match` (`gagneNGP`, `date`, `idUser`) VALUES ('E', current_timestamp(), '" .$_SESSION["idUser"]. "');";
+        $resultat6 = $GLOBALS["pdo"]->query($requete6);
+        $tabuser6 = $resultat6->fetchALL();
         echo "L'ordinateur a gagné !";
       }
     }
     ?>
-
+<p>
+      Bonjour visiteur, vous avez vu cette page <?php echo $_SESSION['count']; ?> fois.
+    </p>
 
   <?php
   } catch (Exception  $error) {
@@ -122,7 +188,7 @@
   ?>
 
 
-
+</div>
   </form>
 </body>
 
