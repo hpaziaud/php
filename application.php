3386c9ff670d4b5e66083ac0e30d367bcd93387b
hpaziaud <paziaud.hassan@gmail.com>
2023-02-08 14:53:52 +0100

    djhfhjhj

--- application.php
+++ application.php
@@ -7,62 +7,10 @@ session_start();
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style-application.css" type="text/css">
   <title>Document</title>
 </head>
-<style>
-      html, body {
-      display: flex;
-      justify-content: center;
-      font-family: Roboto, Arial, sans-serif;
-      font-size: 15px;
-      }
-      form {
-      border: 5px solid #f1f1f1;
-      }
-      input[type=text], input[type=password] {
-      width: 100%;
-      padding: 16px 8px;
-      margin: 8px 0;
-      display: inline-block;
-      border: 1px solid #ccc;
-      box-sizing: border-box;
-      }
-      button {
-      background-color: #8ebf42;
-      color: white;
-      padding: 14px 0;
-      margin: 10px 0;
-      border: none;
-      cursor: grabbing;
-      width: 100%;
-      }
-      h1 {
-      text-align:center;
-      fone-size:18;
-      }
-      button:hover {
-      opacity: 0.8;
-      }
-      .formcontainer {
-      text-align: left;
-      margin: 24px 50px 12px;
-      }
-      .container {
-      padding: 16px 0;
-      text-align:left;
-      }
-      span.psw {
-      float: right;
-      padding-top: 0;
-      padding-right: 15px;
-      }
-      /* Change styles for span on extra small screens */
-      @media screen and (max-width: 300px) {
-      span.psw {
-      display: block;
-      float: none;
-      }
-    </style>
+
 <body>
 
   <?php
@@ -82,9 +30,7 @@ session_start();
     $requete1 = "SELECT match.id as idMatch, match.date,match.gagneNGP,user.id as idUser, user.nom,user.prenom FROM `match`,`user` where match.idUser = user.id";
     $resultat1 = $GLOBALS["pdo"]->query($requete1);
     $tabmatch = $resultat1->fetchALL();
-    foreach ($tabmatch as $match) {
-      echo "id :" . $match["idUser"] . "   on a nom : " . $match["nom"] . " prenom :  " . $match["prenom"] . " a gagné ? " . $match["gagneNGP"] . " le " . $match["date"] . "<br>";
-    }
+   
 
 
 
@@ -98,14 +44,13 @@ session_start();
       $requete = "INSERT INTO `user` (`nom`, `prenom`) VALUES ('" . $_POST['surname'] . "', '" . $_POST["name"] . "')";
       $resultat = $GLOBALS["pdo"]->query($requete);
       $_SESSION["idUser"] = $GLOBALS["pdo"]->lastInsertId();
-      echo "l'id du user est".$_SESSION["idUser"]. "\n";
       $name = $_POST['name'];
 
 
 
-      echo "name = " . $_POST['name'] . " nom = " . $_POST['surname'] . ".";
+      
     } else {
-      echo "ca ne marche pas";
+      echo "";
     }
 
   ?>
@@ -121,27 +66,33 @@ session_start();
     ?>
     
 
-    <h1>HTML Form name Attribute.</h1>
+    
    
     <form action="" method="POST">
     <div class="formcontainer">
+    <h1>play game</h1>
     <hr/>
     <div class="container">
+ 
       First name:
 
-      <input type="text" name="surname">
+      <input type="text" name="surname" required>
       <br> Last name:
 
-      <input type="text" name="name">
+      <input type="text" name="name" required>
       <br>
 
-      <input type="radio" name="choice" value="pierre" />Pierre
+      <input type="radio" name="choice" value="pierre" required/>Pierre
       <input type="radio" name="choice" value="papier" />Papier
       <input type="radio" name="choice" value="ciseaux" />Ciseaux
-      <input type="submit" name="valider" value=''>
-
-    </form>
+      <input type="submit" name="valider" value='submuit value' class="button" required>
     </div>
+    <div class="container" style="background-color: #eee">
+        <label style="padding-left: 15px">
+        
+      </div>
+    </form>
+    
 
     <?php
     if (isset($_POST['valider'])) {
@@ -170,26 +121,33 @@ session_start();
 
         echo "Vous avez gagné !";
       } else {
-        $requete6 = "INSERT INTO `match` (`gagneNGP`, `date`, `idUser`) VALUES ('E', current_timestamp(), '" .$_SESSION["idUser"]. "');";
+        $requete6 = "INSERT INTO `match` (`gagneNGP`, `date`, `idUser`) VALUES ('P', current_timestamp(), '" .$_SESSION["idUser"]. "');";
         $resultat6 = $GLOBALS["pdo"]->query($requete6);
         $tabuser6 = $resultat6->fetchALL();
         echo "L'ordinateur a gagné !";
       }
     }
     ?>
-<p>
-      Bonjour visiteur, vous avez vu cette page <?php echo $_SESSION['count']; ?> fois.
-    </p>
-
+    <?php
+   if (isset($_POST["valider"])){
+   echo "Bonjour id :". $_SESSION["idUser"] ."". $_POST['surname'] ."". $_POST['name'] .", vous avez vu cette page ". $_SESSION['count'] ." fois.";
+  
+   }else{
+echo"coucou";
+   }
+   ?>
+   
   <?php
   } catch (Exception  $error) {
     echo "error est : " . $error->getMessage();
   }
   ?>
+  <?php
+ foreach ($tabmatch as $match) {
+      echo "id : " . $match["idUser"] . "   on a nom : " . $match["nom"] . " prenom :  " . $match["prenom"] . " a gagné ? " . $match["gagneNGP"] . " le " . $match["date"] . "<br>";
+    }
+?>
 
-
-</div>
-  </form>
 </body>
 
 </html>
